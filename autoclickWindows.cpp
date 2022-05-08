2dc3315774baaa5a3af9b8f32e7ff85744075160
krzys <krzys@DESKTOP-7BISF55>
2022-05-08 17:28:28 +0200

    Dodaj pliki projektów.

---
+++ autoclickWindows.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+void clicker(int przerwa)
+{
+	bool click = false; //sets click to false
+
+	while (true)
+	{
+
+		if (GetAsyncKeyState('F')) {
+			mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+			mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+			Sleep(przerwa);
+		}
+		
+	}
+}
+
+int main()
+{
+	int przerwa = 50;
+	cout << "Jaka przerwa wariacie (w milisekundach): ";
+	cin >> przerwa;
+	clicker(przerwa);
+
+	return 0;
+}
